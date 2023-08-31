5b855ba440eac4a140948034ddcb1af5ecd6b563
Richard Sharp <rsharp32@student.cccs.edu>
2023-08-30 18:03:04 -0600

    commit 8302023

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,19 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and goodbye!");
-        System.out.printf("Hello, now go away!");
+        String message1 = "This is";
+        String message2 = "a test";
+        String message3 = "Java is";
+        String message4 = " fun language... when it works.";
+
+        System.out.println(message1);
+        System.out.println(message2);
+        System.out.println(message3 + message4 + "\n");
+
+
+
+
+
 
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
