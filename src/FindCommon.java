77b81e9f36c30239a8c5467fa26832759bc49f7b
Richard Sharp <rsharp32@student.cccs.edu>
2023-10-04 17:48:10 -0600

    project push

---
+++ src/FindCommon.java
@@ -0,0 +1,8 @@
+/*
+Write a program called FindCommon that will call a method that takes two arrays
+as arguments and finds the common elements between them and returns them in
+a third array. Print the array to the screen using Arrays.toString()
+ */
+
+public class FindCommon {
+}
