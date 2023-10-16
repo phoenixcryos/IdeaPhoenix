22dabfb77352478944cd9f647fe18efcc9c3ee6e
phoenixcryos <rsharp32@student.cccs.edu>
2023-10-15 19:11:59 -0600

    final edit- closed scanner and added comments to review later

--- src/arrayReversal.java
+++ src/arrayReversal.java
@@ -3,22 +3,29 @@ import java.util.Scanner;
 public class arrayReversal {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);//scanner initialization
         int[] numbers = new int[100];
         int count = 0;
 
         System.out.println("Enter values (-1 to stop):");
-        int value = scanner.nextInt();
 
-        while (value != -1) {
+
+        while (true) {
+            int value = scanner.nextInt();
+
+            if (value == -1 || count == 100) { //check for stop condition
+                break;
+            }
             numbers[count] = value;
             count++;
-            value = scanner.nextInt();
         }
-
+        //output of input numbers in reverse order
         System.out.println("Your values, reversed:");
         for (int i = count - 1; i >= 0; i--) {
             System.out.println(numbers[i]);
         }
+
+        //closing the scanner
+        scanner.close();
     }
 }
